adc2ff6f75c3e414e789e0c3089ba448b9aeb177
Eoghan O'Keeffe | epok.tech <epok.tech@gmail.com>
2023-12-31 02:32:04 +0000

    Support explainers started, interaction/info updates

--- src/index.js
+++ src/index.js
@@ -3,13 +3,56 @@ import { fit } from '@thi.ng/math/fit';
 import { mix } from '@thi.ng/math/mix';
 import throttle from 'lodash/fp/throttle';
 
-const { min, max, abs, round, ceil, random } = Math;
+const { min, max, abs, round, floor, ceil, random } = Math;
+
+// Progressively load images.
 
 const loaded = ($l) => $l.classList.add('loaded');
 
 each(($l) =>
     (($l.complete)? loaded($l) : $l.addEventListener('load', () => loaded($l))),
   document.querySelectorAll('.load'));
+
+// Figures.
+
+each(($f) => $f.addEventListener('click', () =>
+    ((document.fullscreenElement)? document.exitFullscreen()
+    : $f.requestFullscreen())),
+  document.querySelectorAll('figure'));
+
+// Countdowns to deadlines.
+
+const now = new Date();
+const minute = 60*1e3;
+const hour = 60*minute;
+const day = 24*hour;
+
+function toTil(t, d, h, m) {
+  t -= now;
+  d ??= floor(t/day);
+  h ??= floor((t -= (day*d))/hour);
+  m ??= floor((t -= (hour*h))/minute);
+
+  return `${d && `${d}d`}${h && `:${h}h`}${m && `:${m}m`}`;
+}
+
+const etaCrypto = new Date('2024-01-29T09:00:00-08:00');
+const etaCard = new Date('2024-01-27T09:00:00-08:00');
+
+const fillTil = (til, match) => each(($e) => {
+    const { til: tilTo, title } = $e.dataset;
+
+    $e.textContent = tilTo.replace('$', til);
+    title && ($e.title = title) && $e.classList.add('info');
+  },
+  document.querySelectorAll(match));
+
+fillTil(toTil(etaCrypto), '.eta-crypto');
+fillTil(toTil(etaCard), '.eta-card');
+fillTil(toTil(etaCrypto, null, '', ''), '.eta-crypto-s');
+fillTil(toTil(etaCard, null, '', ''), '.eta-card-s');
+
+// Subscription.
 
 const $subscribe = document.querySelector('#subscribe');
 const $submit = $subscribe.querySelector('[type="submit"]');
@@ -33,6 +76,7 @@ $subscribe.addEventListener('submit', async (e) => {
     });
 });
 
+// Concept art interactions.
 /** @todo [Shrink input to fit value/placeholder](https://stackoverflow.com/a/8100949). */
 
 const $art = document.querySelector('.intro-concept-art');
@@ -65,39 +109,47 @@ $art.addEventListener('pointerout', () => {
   each(($l) => $l.style.clipPath = '', $layers);
 });
 
+// Crypto currency conversion.
+
 (async () => {
   try {
     let c = fetch('https://api.coinconvert.net/convert/eth/usd?amount=0.01');
-    const $c = document.querySelector('.crypto-convert');
-    const { textContent, title } = $c;
 
     c = round((await (await c).json()).USD);
-    $c.textContent = textContent.replace(/(\$)[0-9\.]+/gi, '$1'+c);
-    $c.title = title.replace(/[0-9\.]+( USD)/gi, c+' $1');
+
+    each(($c) => {
+        const { textContent, title } = $c;
+
+        $c.textContent = textContent.replace(/(\$)[0-9\.]+/gi, '$1'+c);
+        $c.title = title.replace(/[0-9\.]+( USD)/gi, c+' $1');
+      },
+      document.querySelectorAll('.crypto-convert'));
   }
   catch(e) { console.warn(e); }
 })();
 
-// const $peer = document.querySelector('.peer');
-// const $peerCamera = $peer.querySelector('.peer-camera');
-// const $peerRandom = $peer.querySelector('.peer-random');
-// const $peerDemo = $peer.querySelector('.peer-demo');
+// Reward: `Peer into the Flow`.
 
-// $peerCamera.addEventListener('change', () => {
-//   const { allow, dataset } = $peerDemo;
-//   const to = dataset[(($peerCamera.checked)? 'y' : 'n')];
+const $peer = document.querySelector('.peer');
+const $peerCamera = $peer.querySelector('.peer-camera');
+const $peerRandom = $peer.querySelector('.peer-random');
+const $peerDemo = $peer.querySelector('.peer-demo');
 
-//   if(allow === to) { return; }
+$peerCamera.addEventListener('change', () => {
+  const { allow, dataset } = $peerDemo;
+  const to = dataset[(($peerCamera.checked)? 'y' : 'n')];
 
-//   $peerDemo.allow = to;
-//   $peerDemo.src = $peerDemo.src;
-// });
+  if(allow === to) { return; }
 
-// $peerRandom.addEventListener('click', () =>
-//   $peerDemo.src = $peerDemo.src.replace(/(^.*\?)(.*$)/, (s, $1, $2) => {
-//     const q = new URLSearchParams($2);
+  $peerDemo.allow = to;
+  $peerDemo.src = $peerDemo.src;
+});
 
-//     q.set('seed', ceil(random()*66));
+$peerRandom.addEventListener('click', () =>
+  $peerDemo.src = $peerDemo.src.replace(/(^.*\?)(.*$)/, (s, $1, $2) => {
+    const q = new URLSearchParams($2);
 
-//     return $1+q;
-//   }));
+    q.set('seed', ceil(random()*66));
+
+    return $1+q;
+  }));
